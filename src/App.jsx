67ee7d229f98e9ce416a40a53b22a839c0612594
Rafael Viana <vianarafaa@gmail.com>
2026-01-31 13:37:53 -0300

    docs: add all the sections files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Contents from './components/Contents'
+import Gallery from './components/Gallery'
+import Partners from './components/Partners'
+import Testimonials from './components/Testimonials'
+import OpenType from './components/OpenType'
+import Footer from './components/Footer'
+
 function App() {
-    return <div></div>
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Features />
+            <Contents />
+            <Gallery />
+            <Partners />
+            <Testimonials />
+            <OpenType />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div>
+            <h1>teste</h1>
+        </div>
+    )
+}
