1be3b3ed0ed15af79709f2eca4a17a74664f85e0
Rafael Viana <vianarafaa@gmail.com>
2026-01-31 15:19:17 -0300

    feat/style: add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 export default function Navbar() {
     return (
-        <div>
-            <h1>teste</h1>
-        </div>
+        <section class="navbar-section">
+            <div>
+                <nav class="flex justify-between ">
+                    <img src="./public/navbar/logo-dark.svg" alt="Figma Land Logo" />
+
+                    <div class="flex gap-3">
+                        <button class="login-button">Login</button>
+                        <img src="./public/navbar/menu-alt-right.svg" alt="Menu image" />
+                    </div>
+                </nav>
+            </div>
+        </section>
     )
 }
