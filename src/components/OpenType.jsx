6fbd605464097b4810a7ad0243a78178ea4192b9
Rafael Viana <vianarafaa@gmail.com>
2026-02-05 11:51:53 -0300

    feat/style: add opentype section

--- src/components/OpenType.jsx
+++ src/components/OpenType.jsx
@@ -1,3 +1,11 @@
 export default function OpenType() {
-    return <div></div>
+    return (
+        <section class="opentype-section" id="opentype-section">
+            <div class="flex text-center flex-col pt-36 px-12">
+                <h1 class="text-5xl pb-14 font-light">OpenType features and Variable fonts</h1>
+                <button class="try-btn font-medium self-center">Try For Free</button>
+            </div>
+            <img src="./public/opentype/lightning-fast.svg" alt="" class="pt-24" />
+        </section>
+    )
 }
