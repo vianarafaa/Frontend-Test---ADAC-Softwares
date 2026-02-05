7f27b489ea475d41aeaed02152bde7640f2a44df
Rafael Viana <vianarafaa@gmail.com>
2026-02-05 12:11:05 -0300

    fix/style: fix opentype css

--- src/components/OpenType.jsx
+++ src/components/OpenType.jsx
@@ -1,11 +1,13 @@
 export default function OpenType() {
     return (
         <section class="opentype-section" id="opentype-section">
-            <div class="flex text-center flex-col pt-36 px-12">
-                <h1 class="text-5xl pb-14 font-light">OpenType features and Variable fonts</h1>
-                <button class="try-btn font-medium self-center">Try For Free</button>
+            <div class=" py-24">
+                <div class="opentype-box flex text-center flex-col pt-24">
+                    <h1 class="text-5xl pb-14 font-light">OpenType features and Variable fonts</h1>
+                    <button class="try-btn font-medium self-center">Try For Free</button>
+                    <img src="./public/opentype/lightning-fast.svg" alt="" class="pt-24" />
+                </div>
             </div>
-            <img src="./public/opentype/lightning-fast.svg" alt="" class="pt-24" />
         </section>
     )
 }
