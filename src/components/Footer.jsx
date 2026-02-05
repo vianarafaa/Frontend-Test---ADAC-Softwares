c1d3d9d5cd0b04e680cb981c61829c53aab681da
Rafael Viana <vianarafaa@gmail.com>
2026-02-05 12:26:30 -0300

    feat/style: add footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,86 @@
 export default function Footer() {
-    return <div></div>
+    return (
+        <section class="footer-section">
+            <footer class="flex text-start flex-col py-24 px-18">
+                <div class="pb-16">
+                    <h3 class="text-xl font-medium pb-10">Fingertipe</h3>
+                    <ul>
+                        <li class="font-medium pb-5">
+                            <a href="">Home</a>
+                        </li>
+                        <li class="font-medium pb-5">
+                            <a href="">Examples</a>
+                        </li>
+                        <li class="font-medium pb-5">
+                            <a href="">Pricing</a>
+                        </li>
+                        <li class="font-medium">
+                            <a href="">Updates</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="pb-16">
+                    <h3 class="text-xl font-medium pb-10">Resources</h3>
+                    <ul>
+                        <li class="font-medium pb-5">
+                            <a href="">Home</a>
+                        </li>
+                        <li class="font-medium pb-5">
+                            <a href="">Examples</a>
+                        </li>
+                        <li class="font-medium pb-5">
+                            <a href="">Pricing</a>
+                        </li>
+                        <li class="font-medium">
+                            <a href="">Updates</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="pb-16">
+                    <h3 class="text-xl font-medium pb-10">About</h3>
+                    <ul>
+                        <li class="font-medium pb-5">
+                            <a href="">Home</a>
+                        </li>
+                        <li class="font-medium pb-5">
+                            <a href="">Examples</a>
+                        </li>
+                        <li class="font-medium pb-5">
+                            <a href="">Pricing</a>
+                        </li>
+                        <li class="font-medium">
+                            <a href="">Updates</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="flex pb-2.5">
+                        <img src="./public/footer/pin.svg" alt="" class="w-9" />
+                        <p class="font-medium self-center">7480 Mockingbird Hill undefined </p>
+                    </div>
+                    <div class="flex">
+                        <img src="./public/footer/phone.svg" alt="" class="w-8" />
+                        <p class="font-medium self-center">(239) 555-0108</p>
+                    </div>
+                    <ul class="flex pt-7 gap-6">
+                        <li class="">
+                            <a href="">
+                                <img src="./public/footer/twitter.svg" alt="" />
+                            </a>
+                        </li>
+                        <li class="">
+                            <a href="">
+                                <img src="./public/footer/facebook.svg" alt="" />
+                            </a>
+                        </li>
+                        <li class="">
+                            <a href="">
+                                <img src="./public/footer/linkedin.svg" alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+        </section>
+    )
 }
