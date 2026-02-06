13ce8070beefa144a7816ac0fac5f28cee1ca3f9
Rafael Viana <vianarafaa@gmail.com>
2026-02-06 16:42:32 -0300

    style: add responsivity to the testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ export default function Testimonials() {
                 <div class="testimonials-texts">
                     <h1 class="text-5xl pb-14 font-light">Testimonials</h1>
                 </div>
-                <div class="testimonials-cards flex gap-6 flex-col">
+                <div class="testimonials-cards flex gap-6 flex-col lg:hidden">
                     <div class="testimonial-card flex flex-col p-10 justify-start rounded-xl">
                         <div class="flex flex-row text-left pb-4">
                             <img src="/testimonials/avatar-mb1.svg" alt="" />
@@ -60,6 +60,63 @@ export default function Testimonials() {
                             Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
                             Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
                             your financial goal for the month and year.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="hidden lg:grid grid-cols-[400px_400px] justify-center gap-5">
+                    <div class="testimonial-card flex flex-col p-10 rounded-xl">
+                        <div class="flex flex-row text-left pb-4">
+                            <img src="/testimonials/avatar.svg" alt="" />
+                            <div class="font-medium pl-6">
+                                <h2>Claire Bell</h2>
+                                <h3>Designer</h3>
+                            </div>
+                        </div>
+                        <p class="font-light text-left">
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.your
+                            financial goal for the month and year.
+                        </p>
+                    </div>
+                    <div class="testimonial-card flex flex-col p-10 rounded-xl">
+                        <div class="flex flex-row text-left pb-4">
+                            <img src="/testimonials/avatar-1.svg" alt="" />
+                            <div class="font-medium pl-6">
+                                <h2>Francisco Lane</h2>
+                                <h3>Designer</h3>
+                            </div>
+                        </div>
+                        <p class="font-light text-left">
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
+                        </p>
+                    </div>
+
+                    <div class="testimonial-card flex flex-col p-10 rounded-xl">
+                        <div class="flex flex-row text-left pb-4">
+                            <img src="/testimonials/avatar-2.svg" alt="" />
+                            <div class="font-medium pl-6">
+                                <h2>Ralph Fisher</h2>
+                                <h3>Designer</h3>
+                            </div>
+                        </div>
+                        <p class="font-light text-left">
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
+                        </p>
+                    </div>
+                    <div class="testimonial-card flex flex-col p-10 rounded-xl">
+                        <div class="flex flex-row text-left pb-4">
+                            <img src="/testimonials/avatar-3.svg" alt="" />
+                            <div class="font-medium pl-6">
+                                <h2>Jorge Murphy</h2>
+                                <h3>Designer</h3>
+                            </div>
+                        </div>
+                        <p class="font-light text-left">
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
+                            Slate helps you see how many more days you need to work to reach your financial goal for the month and year.
                         </p>
                     </div>
                 </div>
